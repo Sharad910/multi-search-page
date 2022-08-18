9eed4f8dfd95a30f538e039fdf4fc9a0c319c45a
Sharad Arora <f20211427@goa.bits-pilani.ac.in>
2022-08-18 14:51:41 +0530

    responsiveness

--- app.js
+++ app.js
@@ -7,37 +7,31 @@ let url= new URL('https://www.google.com/search');
 
 input.addEventListener('input',function(e){
     if(input.value.toLowerCase().startsWith('youtube ')){
-        searchengine.innerText='Search Youtube';
         url =new URL('https://www.youtube.com/results');
         url.searchParams.set('search_query',`${input.value.slice(8)}`);
         icon.setAttribute('src','./imgs/youtube.jpg');
     }
     else if(input.value.toLowerCase().startsWith('github ')){
-        searchengine.innerText='Search Github';
         url =new URL('https://github.com/search');
         url.searchParams.set('q',`${input.value.slice(7)}`);
         icon.setAttribute('src','./imgs/github.jpg');
     }
     else if(input.value.toLowerCase().startsWith('docs ')){
-        searchengine.innerText='Search Docs';
         url= new URL('https://www.google.com/search');
         url.searchParams.set('q',`${'site:docs.google.com '+`${input.value.slice(5)}`}`);
         icon.setAttribute('src','./imgs/docs.jpg');
     }
     else if(input.value.startsWith('slides ')){
-        searchengine.innerText='Search Slides';
         url= new URL('https://www.google.com/search');
         url.searchParams.set('q',`${'site:docs.google.com/presentation '+`${input.value.slice(7)}`}`);
         icon.setAttribute('src','./imgs/slides.jpg');
     }
     else if(input.value.startsWith('sheets ')){
-        searchengine.innerText='Search Sheets';
         url= new URL('https://www.google.com/search');
         url.searchParams.set('q',`${'site:docs.google.com/spreadsheets '+`${input.value.slice(7)}`}`);
         icon.setAttribute('src','./imgs/sheets.png');
     }
     else {
-        searchengine.innerText='Search Google';
         url= new URL('https://www.google.com/search');
         url.searchParams.set('q',`${input.value}`);
         icon.setAttribute('src','./imgs/google.png');
